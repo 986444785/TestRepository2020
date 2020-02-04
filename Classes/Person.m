ab2fcc3003bb2452dfde3a202bf22cf3053c074c
cwb <>
2020-02-04 16:48:34 +0800

    0.0.2 版本提交

--- Classes/Person.m
+++ Classes/Person.m
@@ -10,4 +10,20 @@
 
 @implementation Person
 
+
+-(void)descriptionLog{
+    //
+    NSLog(@"Copyright © 2020 Evon2020. All rights reserved. 2020/2/1. ");
+    
+    
+    
+    NSLog(@"学习地址 https://www.cnblogs.com/weipeng168/p/9598190.html    更新地址 https://blog.csdn.net/yaoliangjun306/article/details/73550902");
+    
+    
+    //pod trunk  register 986444785@qq.com "986444785" --verbose
+
+}
+
+
+
 @end
